bffb767f76dc833c598d7883a0a4d52fa3010098
Alexis Venegas <alexisvfilmstuff@protonmail.com>
2021-09-03 21:06:55 -0700

    created questions array

--- develop/index.js
+++ develop/index.js
@@ -1,10 +1,56 @@
+// properties needed to use path, inquirer, and fs to generate readme and interact with generateMarkdown
 const path = require('path')
 const inquirer = require('inquirer')
 const fs = require('fs')
 const generateMarkdown = require('./utils/generateMarkdown')
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your github user name?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?'
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'What is the description of your project?'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What is the licenses should your project have?',
+    choices: ['MIT', 'Apache-2.0', 'BSD-3', 'GPL-3.0', 'NONE']
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How does the user install this project?'
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command does the user run, to run tests?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know to use the repo?'
+  },
+  {
+    type: 'input',
+    name: 'contribute',
+    message: 'What does the user need to know about contributing to the repo?'
+  },
 ];
 
 // function to write README file
